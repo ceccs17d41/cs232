7023c840f48612f837d33393f822728a080098f9
ceccs17d41 <43229596+ceccs17d41@users.noreply.github.com>
2019-05-11 17:31:18 +0530

    Add files via upload

---
+++ Task8-KernelCompilation/kernel.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+curl -fLO "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.0.9.tar.xz"
+sudo apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison
+tar xvzf linux-5.0.9.tar.xz 
+cd linux-5.0.9
+cp /boot/config-4.19.0-kali4-amd64 .config
+make menuconfig
+make
+make modules_install
+sudo make install
+
