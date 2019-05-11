51c371d0c8e5226331cdc92eda4c5f48e2632ecc
ceccs17d41 <43229596+ceccs17d41@users.noreply.github.com>
2019-05-11 17:34:46 +0530

    Add files via upload

---
+++ Task8-PackageManagement/package_management.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo apt list --upgradable
+sudo apt list --installed
+sudo apt list --all-versions
+sudo apt autoremove
+sudo apt upgrade
+sudo apt search vlc
+sudo apt install vlc
+sudo apt show vlc
+sudo apt full-upgrade
+sudo apt remove vlc
+sudo apt install vlc
+sudo apt purge vlc
+
